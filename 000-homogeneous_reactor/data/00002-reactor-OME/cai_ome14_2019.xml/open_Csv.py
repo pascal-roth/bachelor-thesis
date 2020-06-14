8ea0bfc62be0cbc266b862a172090538e58a61fd
pascal.roth.18 <pascal.roth@stud.tu-darmstadt.de>
2020-06-14 09:05:42 +0200

    open file modification

--- 000-homogeneous_reactor/data/00002-reactor-OME/cai_ome14_2019.xml/open_Csv.py
+++ 000-homogeneous_reactor/data/00002-reactor-OME/cai_ome14_2019.xml/open_Csv.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-samples_normal = pd.read_csv('005_train_samples.csv')
-delays_normal = pd.read_csv('005_train_delays.csv')
-samples_multi = pd.read_csv('006_train_samples.csv')
-delays_multi = pd.read_csv('006_train_delays.csv')
+samples_normal = pd.read_csv('000_train_samples.csv')
+delays_normal = pd.read_csv('000_train_delays.csv')
 
+print(samples_normal.shape)
 
+print(samples_normal.iloc[len(samples_normal)-10000000, :])
