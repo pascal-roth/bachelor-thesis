76b2b91223edbd4da8437186553fe07d948a1d8c
Roth, Pascal <pascal.roth.18@stud.tu-darmstadt.de>
2020-07-13 00:08:03 +0200

    contour plot bugfixe

--- 001-machine_learning/code/00001-MLP-temperature/contour.py
+++ 001-machine_learning/code/00001-MLP-temperature/contour.py
@@ -8,94 +8,65 @@ from fc_pre_processing_load import load_samples, normalize_df
 from fc_post_processing import load_checkpoint
 import torch
 import numpy as np
-import cantera as ct
+from pathlib import Path
 import matplotlib.pyplot as plt
 plt.style.use('stfs')
 
+
 #%% get arguments
-# parser = argparse.ArgumentParser(description="Create contour plot")
-#
-# parser.add_argument("-nbr_net", "--number_net", type=str, default='000',
-#                     help="chose number of the network")
-#
-# parser.add_argument("-phi", "--equivalence_ratio", nargs='+', type=float, default=[0.0],
-#                     help="chose equivalence ratio")
-#
-# parser.add_argument("-Z", "--mixture_fraction", type=float, default=[0.0],
-#                     help="chose mixture fraction")
-#
-# parser.add_argument("--pode", type=int, nargs='+', default=[0],
-#                     help="chose degree of polymerization")
-#
-# parser.add_argument("-inf_print", "--information_print", default=True, action='store_false',
-#                     help="chose if basic information are displayed")
-#
-# args = parser.parse_args()
-# if args.information_print is True:
-#     print('\n{}\n'.format(args))
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Create contour plot")
+
+    parser.add_argument("-mech", "--mechanism_input", type=str, choices=['he', 'sun', 'cai'], default='cai',
+                        help="chose reaction mechanism")
+
+    parser.add_argument("-nbr_net", "--number_net", type=str, default='008',
+                        help="chose number of the network")
+
+    parser.add_argument("-phi", "--equivalence_ratio", nargs='+', type=float, default=[1.0],
+                        help="chose equivalence ratio")
+
+    parser.add_argument("--pode", type=int, nargs='+', default=[3],
+                        help="chose degree of polymerization")
+
+    parser.add_argument("-p", "--pressure", nargs='+', type=int, default=[20],
+                        help="chose reactor pressure")
+
+    parser.add_argument("-inf_print", "--information_print", default=True, action='store_false',
+                        help="chose if basic information are displayed")
+
+    args = parser.parse_args()
+    if args.information_print is True:
+        print('\n{}\n'.format(args))
+
+    return args
 
 
-mechanism_input = 'cai'
-equivalence_ratio = [1.0]
-pode = [3]
-number_net = '002'
+#%% functions
+def get_y_samples(x_samples, x_scaler, y_samples):
+    # output of the MLP
+    x_samples_normalized, _ = normalize_df(x_samples, x_scaler)
+    x_samples_normalized = torch.tensor(x_samples_normalized.values).float()
+    model.eval()
+    y_samples_nn = model.forward(x_samples_normalized)
+    y_samples_nn = y_samples_nn.detach().numpy()
+    y_samples_nn = y_scaler.inverse_transform(y_samples_nn)
 
-# Load MLP checkpoint --> get model, used training set and features/labels
-model, criterion, features, labels, x_scaler, y_scaler, n_input, n_output, number_train_run = \
-    load_checkpoint(number_net)
+    # Calculate the difference between reactor and MLP output
+    y_samples_diff = y_samples - y_samples_nn
 
-# Load training set
-feature_select = {'pode': pode, 'phi': equivalence_ratio, 'P_0': [20 * ct.one_atm]}
+    return y_samples_nn, y_samples_diff
 
 
-x_samples, y_samples = load_samples(mechanism_input, number_train_run, feature_select, features=features,
-                                    labels=labels, select_data='include', category='train')
-
-# output of the MLP
-x_samples_normalized, _ = normalize_df(x_samples, x_scaler)
-x_samples_normalized = torch.tensor(x_samples_normalized.values).float()
-model.eval()
-y_samples_nn = model.forward(x_samples_normalized)
-y_samples_nn = y_samples_nn.detach().numpy()
-y_samples_nn = y_scaler.inverse_transform(y_samples_nn)
-
-# Calculate the difference between reactor and MLP output
-y_samples_diff = y_samples - y_samples_nn
-
-# %% Scatter plot
-plt_scatter = True
-
-if plt_scatter:
-    x = x_samples[['PV']]
-    y = x_samples[['H']] / 1.e+6
-    z = y_samples
-
-    fig, axs = plt.subplots(nrows=1, ncols=2)
-
-    axs[0].scatter(x, y) #, s=area, c=colors, alpha=0.5)
-    axs[0].set_title('Energy over OV')
-    axs[0].set_xlabel('PV')
-    axs[0].set_ylabel('H [MJ/kg]')
-
-    axs[1].scatter(x, z) #, s=area, c=colors, alpha=0.5)
-    axs[1].set_title('Temperature over PV')
-    axs[1].set_xlabel('PV')
-    axs[1].set_ylabel('T [K]')
-
-    plt.show()
-
-# %% Contour Plot
-interpolate = True
-
-if interpolate:
+def create_grid(x_samples, y_samples, y_samples_nn, y_samples_diff):
     PV_max = np.amax(x_samples[['PV']])
     PV_min = np.amin(x_samples[['PV']])
 
-    x_samples[['H']] = x_samples[['H']].round(-3)
-    U_max = np.amax(x_samples[['H']])
-    U_min = np.amin(x_samples[['H']])
+    # x_samples[['H']] = x_samples[['H']].round(-3)
+    H_max = np.amax(x_samples[['H']])
+    H_min = np.amin(x_samples[['H']])
 
-    grid_x, grid_y = np.mgrid[PV_min[0]:PV_max[0]:7000j, U_min[0]:U_max[0]:200j]
+    grid_x, grid_y = np.mgrid[PV_min[0]:PV_max[0]:7000j, H_min[0]:H_max[0]:200j]
 
     from scipy.interpolate import griddata
 
@@ -107,59 +78,100 @@ if interpolate:
     grid_nn = np.squeeze(grid_nn)
     grid_diff = np.squeeze(grid_diff)
 
-else:  # manually create grid
-    Us = x_samples[['H']].round(-5)
-    Us = Us.drop_duplicates()
-    indexes = Us.index.values
+    return grid_x, grid_y, grid_reactor, grid_nn, grid_diff
 
-    grid_reactor = np.zeros((len(Us), 7000))
-    grid_nn = np.zeros((len(Us), 7000))
-    grid_diff = np.zeros((len(Us), 7000))
-    grid_x = np.zeros((len(Us), 7000))
-    grid_y = np.zeros((len(Us), 7000))
 
-    n = 0
+def plotter(x_samples, grid_x, grid_y, grid_reactor, grid_nn, grid_diff, number_net, label, equivalence_ratio,
+            pressure):
+    grid_y = grid_y / 1.e6
+    Z = x_samples[['Z']].iloc[0]
 
-    for i in range(len(Us)):
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=[15, 5])
 
-        if not i == len(Us)-1:
-            n_samples = indexes[i+1] - indexes[i]
-        else:
-            n_samples = x_samples.index.max() - indexes[i]
+    img = axs[0].contourf(grid_x, grid_y, grid_reactor, levels=100, cmap='gist_heat_r')
+    axs[0].set_xlabel('PV')
+    axs[0].set_ylabel('h [MJ/kg]')
 
-        grid_reactor[i, :n_samples] = np.ravel(y_samples.iloc[n:(n+n_samples)])
-        grid_nn[i, :n_samples] = np.ravel(y_samples_nn[n:(n+n_samples)])
-        grid_diff[i, :n_samples] = np.ravel(y_samples_diff.iloc[n:(n+n_samples)])
+    fig.colorbar(img, ax=axs[0], label='{}_reactor'.format(label))
+    axs[0].set_title('Z={:.2f}'.format(Z[0]))
 
-        grid_x[i, :n_samples] = np.ravel(x_samples[['PV']].iloc[n:(n+n_samples)])
-        grid_y[i, :n_samples] = np.ravel(x_samples[['H']].iloc[n:(n+n_samples)])
+    img = axs[1].contourf(grid_x, grid_y, grid_nn, levels=100, cmap='gist_heat_r')
+    axs[1].set_xlabel('PV')
+    axs[1].set_ylabel('h [MJ/kg]')
+    fig.colorbar(img, ax=axs[1], label='{}_MLP'.format(label))
+    axs[1].set_title('Z={:.2f}'.format(Z[0]))
 
-        n += n_samples
+    img = axs[2].contourf(grid_x, grid_y, grid_diff, levels=100, cmap='gist_heat_r')
+    axs[2].set_xlabel('PV')
+    axs[2].set_ylabel('h [MJ/kg]')
+    fig.colorbar(img, ax=axs[2], label='{}_diff'.format(label))
+    axs[2].set_title('Z={:.2f}'.format(Z[0]))
 
-#%% create plot
-grid_y = grid_y / 1.e6
-Z = x_samples[['Z']].iloc[0]
+    # plt.tight_layout()
 
-fig, axs = plt.subplots(nrows=1, ncols=3, figsize=[15, 5])
+    path = Path(__file__).resolve()
+    path_plt = path.parents[2] / 'data/00001-MLP-temperature/{}_plt_{}_contour_{}_{}.pdf'.format \
+        (number_net, label, equivalence_ratio, pressure)
 
-img = axs[0].contourf(grid_x, grid_y, grid_reactor, levels=100, cmap='gist_heat_r')
-axs[0].set_xlabel('PV')
-axs[0].set_ylabel('h [MJ/kg]')
+    plt.savefig(path_plt)
 
-fig.colorbar(img, ax=axs[0], label='T_reactor [K]')
-axs[0].set_title('Z={:.2f}'.format(Z[0]))
+    plt.show()
 
-img = axs[1].contourf(grid_x, grid_y, grid_nn, levels=100, cmap='gist_heat_r')
-axs[1].set_xlabel('PV')
-axs[1].set_ylabel('h [MJ/kg]')
-fig.colorbar(img, ax=axs[1], label='T_MLP [K]')
-axs[1].set_title('Z={:.2f}'.format(Z[0]))
 
-img = axs[2].contourf(grid_x, grid_y, grid_diff, levels=100, cmap='gist_heat_r')
-axs[2].set_xlabel('PV')
-axs[2].set_ylabel('h [MJ/kg]')
-fig.colorbar(img, ax=axs[2], label='T_diff [K]')
-axs[2].set_title('Z={:.2f}'.format(Z[0]))
+#%%
+if __name__ == "__main__":
 
-# plt.tight_layout()
-plt.show()
+    args = parseArgs()
+
+    print('Load model ...')
+
+    # Load MLP checkpoint --> get model, Hsed training set and features/labels
+    model, criterion, features, labels, x_scaler, y_scaler, n_input, n_output, number_train_run = \
+        load_checkpoint(args.number_net)
+
+    print('Model loaded, load samples ...')
+
+    # Load training set
+    feature_select = {'pode': args.pode, 'phi': args.equivalence_ratio, 'P_0': args.pressure}
+
+    x_samples, y_samples = load_samples(args.mechanism_input, number_train_run, feature_select, features=features,
+                                        labels=labels, select_data='include', category='train')
+
+    print('DONE!, Create Plot ...')
+    
+    y_samples_nn, y_samples_diff = get_y_samples(x_samples, x_scaler, y_samples)
+
+    for i, label in enumerate(labels):
+        y_samples_run = y_samples.iloc[:, i]
+        y_samples_nn_run = y_samples_nn[:, i]
+        y_samples_diff_run = y_samples_diff.iloc[:, i]
+
+        grid_x, grid_y, grid_reactor, grid_nn, grid_diff = create_grid(x_samples, y_samples_run, y_samples_nn_run,
+                                                                       y_samples_diff_run)
+
+        plotter(x_samples, grid_x, grid_y, grid_reactor, grid_nn, grid_diff, args.number_net, label,
+                args.equivalence_ratio, args.pressure)
+
+
+
+# # %% Scatter plot
+# plt_scatter = False
+#
+# if plt_scatter:
+#     x = x_samples[['PV']]
+#     y = x_samples[['H']] / 1.e+6
+#     z = y_samples
+#
+#     fig, axs = plt.subplots(nrows=1, ncols=2)
+#
+#     axs[0].scatter(x, y) #, s=area, c=colors, alpha=0.5)
+#     axs[0].set_title('Energy over OV')
+#     axs[0].set_xlabel('PV')
+#     axs[0].set_ylabel('H [MJ/kg]')
+#
+#     axs[1].scatter(x, z) #, s=area, c=colors, alpha=0.5)
+#     axs[1].set_title('Temperature over PV')
+#     axs[1].set_xlabel('PV')
+#     axs[1].set_ylabel('T [K]')
+#
+#     plt.show()
---
+++ 001-machine_learning/code/00001-MLP-temperature/contour.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /home/pascal/anaconda3/bin/activate BA
+
+echo "Welcome to contour plot generator"
+echo "---------------------------------"
+echo ""
+
+echo "Select parameters"
+read -p "number of MLP         [XXX]:                 " number_net
+read -p "PODE_n                [1, 2, 3, 4]:          " pode_n
+read -p "equivalence ratio     (0.5, 1.5, 0.05):      " phi
+read -p "pressure              ( 10,  20,    2):      " pressure
+
+python contour.py --pode $pode_n -nbr_net $number_net -phi $phi -p $pressure; exit
